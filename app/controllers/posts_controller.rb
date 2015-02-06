ad844e1c0dcb94acc42385935c43aa25540f85ad
ryan <lrcrews@gmail.com>
2015-02-05 21:51:44 -0800

    if no posts exist show the meta post when random post link is pressed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -47,7 +47,7 @@ class PostsController < ApplicationController
 	private
 
 		def set_random_post_link
-			gon.random_post_link = post_url( id: rand(1..Post.count) )
+			gon.random_post_link = params[:id].present? ? post_url( id: rand(1..Post.count) ) : meta_posts_url
 		end
 
 
